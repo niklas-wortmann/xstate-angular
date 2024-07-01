82f2557fb45c264cff22c8010ded03dd559a849f
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-07-01 14:48:06 -0500

    repository setup

---
+++ lib/angular-xstate/src/index.ts
@@ -0,0 +1,4 @@
+export { useActor } from './lib/useActor';
+export { useActorRef } from './lib/useActorRef';
+export { useMachine } from './lib/useMachine';
+export { useSelector } from './lib/useSelector';
