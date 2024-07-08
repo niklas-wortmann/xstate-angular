0b48b6c70b1017d4b48a97dbf852c5df74b406a0
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-07-08 14:25:45 -0500

    docs: improve API docs

--- lib/xstate-ngx/src/lib/useActor.ts
+++ lib/xstate-ngx/src/lib/useActor.ts
@@ -34,11 +34,11 @@ export function useActor<TLogic extends AnyActorLogic>(
   @Injectable({ providedIn: providedIn ?? null })
   class ActorStore implements ActorStoreProps<TLogic> {
     public actorRef: Actor<TLogic>;
-    private _snapshot: WritableSignal<SnapshotFrom<TLogic>>;
     public send: Actor<TLogic>['send'];
     public snapshot: Signal<SnapshotFrom<TLogic>>;
 
-    private isMachine = true;
+    private _isMachine = true;
+    private _snapshot: WritableSignal<SnapshotFrom<TLogic>>;
 
     constructor() {
       const listener = (nextSnapshot: Snapshot<unknown>) => {
@@ -49,13 +49,13 @@ export function useActor<TLogic extends AnyActorLogic>(
       this._snapshot = signal(this.actorRef.getSnapshot());
       this.send = this.actorRef.send;
       this.snapshot = this._snapshot.asReadonly();
-      this.isMachine = isMachineSnapshot(this.snapshot());
+      this._isMachine = isMachineSnapshot(this.snapshot());
     }
 
     matches(path: MatchType<TLogic>) {
       return computed(() => {
         const machineSnapshot = this.snapshot();
-        if (this.isMachine) {
+        if (this._isMachine) {
           return (machineSnapshot as AnyMachineSnapshot).matches(path);
         }
         return false;
@@ -65,7 +65,7 @@ export function useActor<TLogic extends AnyActorLogic>(
     hasTag(path: HasTagType<TLogic>) {
       return computed(() => {
         const machineSnapshot = this.snapshot();
-        if (this.isMachine) {
+        if (this._isMachine) {
           return (machineSnapshot as AnyMachineSnapshot).hasTag(path);
         }
         return false;
@@ -75,7 +75,7 @@ export function useActor<TLogic extends AnyActorLogic>(
     can(path: CanType<TLogic>) {
       return computed(() => {
         const machineSnapshot = this.snapshot();
-        if (this.isMachine) {
+        if (this._isMachine) {
           return (machineSnapshot as AnyMachineSnapshot).can(path);
         }
         return false;
@@ -95,10 +95,37 @@ type CanType<TLogic extends AnyActorLogic> = FnParameter<TLogic, 'can'>;
 type HasTagType<TLogic extends AnyActorLogic> = FnParameter<TLogic, 'hasTag'>;
 
 export interface ActorStoreProps<TLogic extends AnyActorLogic> {
+  /**
+   * Represents a reference to an actor.
+   */
   actorRef: Actor<TLogic>;
+  /**
+   * Represents a snapshot of the actor as Signal.
+   */
   snapshot: Signal<SnapshotFrom<TLogic>>;
+  /**
+   * Sends a message to the actor associated with the specified logic.
+   */
   send: Actor<TLogic>['send'];
+  /**
+   * Returns a computed value indicating whether the provided path matches the machine snapshot.
+   *
+   * @param {MatchType<TLogic>} path - The path to be matched against the machine snapshot.
+   * @return {Signal<boolean>} - A computed boolean value indicating whether the path matches the machine snapshot.
+   */
   matches: (path: MatchType<TLogic>) => Signal<boolean>;
+  /**
+   * Determines whether the specified path can be transitioned to in the state machine.
+   *
+   * @param {CanType<TLogic>} path - The path to check if it can be transitioned to.
+   * @return {Signal<boolean>} - Returns true if the specified path can be transitioned to, otherwise returns false.
+   */
   can: (event: CanType<TLogic>) => Signal<boolean>;
+  /**
+   * Determines if the machine or machine snapshot has the specified tag.
+   *
+   * @param {HasTagType<TLogic>} path - The tag path.
+   * @return {Signal<>boolean>} - True if the machine or machine snapshot has the tag, otherwise false.
+   */
   hasTag: (event: HasTagType<TLogic>) => Signal<boolean>;
 }
